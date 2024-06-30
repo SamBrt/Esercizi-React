a8e4a6e0674c3c165bc788c58bf6862ada2a8e6a
Samuele Berti <samuelebrt4@gmail.com>
2024-06-30 17:32:38 +0200

    clock added

---
+++ UseEffect/src/App.jsx
@@ -0,0 +1,15 @@
+import { Clock } from "./Clock.jsx";
+import Hello from "./Hello.jsx";
+
+
+
+function App(){
+  return (
+    <div>
+      <Hello/>
+      <Clock/>
+    </div>
+  )
+}
+
+export default App
